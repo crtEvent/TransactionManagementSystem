4b98b376a4230d392c7fd069478ff082cf159d53
jshse <jshse@192.168.0.138>
2022-09-14 22:22:40 +0900

    feat: ip추적 interceptor 기능 추가
     * 접속자의 ip를 추적하기 위한 interceptor 추가

---
+++ SSHTransactionManagement/src/main/java/com/sshmanager/ssh/common/interceptor/IpTracker.java
@@ -0,0 +1,48 @@
+package com.sshmanager.ssh.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class IpTracker  extends HandlerInterceptorAdapter {
+
+	protected Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		if (log.isDebugEnabled()) {
+			log.debug("======================================          START         ======================================");
+			log.debug("[Request URI] \t: " + request.getRequestURI());
+			
+			String ip = request.getHeader("X-FORWARDED-FOR"); 
+	         
+	         //proxy 환경일 경우
+	         if (ip == null || ip.length() == 0) {
+	             ip = request.getHeader("Proxy-Client-IP");
+	         }
+	 
+	         //웹로직 서버일 경우
+	         if (ip == null || ip.length() == 0) {
+	             ip = request.getHeader("WL-Proxy-Client-IP");
+	         }
+	 
+	         if (ip == null || ip.length() == 0) {
+	             ip = request.getRemoteAddr() ;
+	         }
+	         
+	         log.debug("[IP] \t: " + ip);
+		}
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		if (log.isDebugEnabled()) {
+			log.debug("======================================           END          ======================================\n");
+		}
+ 	}
+}
